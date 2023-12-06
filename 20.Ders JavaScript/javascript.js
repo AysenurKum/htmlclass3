aaeb98345f686b245632afe47ed9fc312715ca79
AysenurKum <nurlice.61@outlook.com>
2023-12-06 22:43:44 +0300

    20.DERS

---
+++ 20.Ders JavaScript/javascript.js
@@ -0,0 +1,69 @@
+// function myFunction3() {
+//     document.getElementById("demo").innerHTML="External script çalıştı"
+// }
+
+var x = 5 ;
+var y = 6 ;
+x = x + 5;
+var z = x + y ;
+console.log(z)
+
+//  const değişmeyen ifadeler için kullanılır 
+
+const price1 = 5;
+const price2 = 6;
+let total = price1 + price2;
+// total = 40;
+
+console.log(total)
+
+console.log(x == x + 5)
+
+const pi= 3.14 ;
+let person = "Mustafa"
+console.log(person)
+
+var carName1;
+let carName2;
+carName2 = "volvo"
+let carName3 = "Mercedes"
+console.log(carName2)
+let person4 = "ahmet", carName4 = "Renalt" , price4 = 200 ;
+
+
+let carName = "Volvo"
+
+var carName5 = "Volvo";
+var carName5 = "Fiat";
+console.log(carName5)
+
+let a1 = 2 ;
+{
+    let a1 = 5 ;
+    console.log(a1)
+}
+console.log(a1)
+
+{
+    var a2 = 3 ;
+}
+a2 = 88 ;
+console.log(a2)
+
+// carName6 = "BMW"
+// var carname6;
+// let carName6 = "Volvo"
+// console.log(carName6)
+
+// dizi []
+const cars = ["Fiat","Toyota","Audi"]
+const numbers = [1,2,3,4,5,6,7,8,9]
+car[2] = "BMW"
+console.log(cars[0])
+console.log(cars)
+
+// object {}
+const car1 = {type:"Fiat",model:"500",color:"white" ,age:"15"}
+console.log(car1.color)
+
+
